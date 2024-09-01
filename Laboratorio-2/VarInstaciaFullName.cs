f742f497462b07eb3e63a9896282c85228df6e3a
jeansuarex <jeansuarex12@gmail.com>
2024-09-01 01:38:44 -0500

    Add Laboratorio 2

---
+++ Laboratorio-2/VarInstaciaFullName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Laboratorio_2
+{
+    internal class VarInstaciaFullName
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.FirstName = "Jean";
+            client.LastName = "Suarez";
+            client.Age = 20;
+            client.Id = 1;
+
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Laboratorio-2/VarLocalSum.cs
@@ -0,0 +1,23 @@
+﻿class VarLocalSum
+{
+    static void Main(string[] args)
+    {
+        VarLocalSum program = new VarLocalSum();
+
+        program.Sumar(5, 10);         
+        program.SumarYMostrar(3, 7);  
+    }
+
+    public void Sumar(int valor1, int valor2)
+    {
+        int variableLocal = valor1 + valor2;
+        Console.WriteLine(variableLocal);
+    }
+
+    public void SumarYMostrar(int valor1, int valor2)
+    {
+        int variableLocal = valor1 + valor2;
+        Console.WriteLine(variableLocal);
+        Console.WriteLine(valor1);
+    }
+}
