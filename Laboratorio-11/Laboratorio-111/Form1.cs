fb15e166e6255df7d72bbb0a232b1f0f90183f9f
jeansuarex <jeansuarex12@gmail.com>
2024-10-02 21:26:45 -0200

    Laboratorio 11 terminado

---
+++ Laboratorio-11/Laboratorio-111/Form1.cs
@@ -0,0 +1,19 @@
+namespace Laboratorio_111
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Hola Mundo";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
